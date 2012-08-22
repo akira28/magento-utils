fe74483be057ef98e138078c9f9e24c94f8cfbcb
akira28 <andreadepirro@gmail.com>
2012-08-22 09:51:05 +0200

    added obfuscateUsers sql script to obfuscate mails and names of a magento db

---
+++ obfuscateUsers.sql
@@ -0,0 +1,5 @@
+UPDATE `customer_entity` SET `email` = CONCAT ( md5(`email`) ,"@", "example.com");
+UPDATE `sales_flat_order` SET `customer_email` = CONCAT ( SUBSTR(md5(`customer_email`),6) ,"@", "example.com") , `customer_firstname` = md5(`customer_firstname`) , `customer_lastname` = md5(`customer_lastname`);
+UPDATE `sales_flat_order_address` SET `email` = CONCAT ( SUBSTR(md5(`email`),6) ,"@", "example.com") , `firstname` = md5(`firstname`) , `lastname` = md5(`lastname`);
+UPDATE `sales_flat_quote` SET `customer_email` = CONCAT ( SUBSTR(md5(`customer_email`),6) ,"@", "example.com") , `customer_firstname` = md5(`customer_firstname`) , `customer_lastname` = md5(`customer_lastname`);
+UPDATE `sales_flat_quote_address` SET `email` = CONCAT ( SUBSTR(md5(`email`),6) ,"@", "example.com") , `firstname` = md5(`firstname`) , `lastname` = md5(`lastname`);
