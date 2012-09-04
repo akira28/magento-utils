ecf9442a45e9dd9e930cd9baeb28092c3e4027ba
andrea <andrea@andrea.newshore>
2012-09-04 17:34:53 +0200

    SQL beautification

--- sql/disableProducts.sql
+++ sql/disableProducts.sql
@@ -1,6 +1,9 @@
 # @author      Andrea De Pirro <andrea.depirro@yameveo.com>
 
-UPDATE catalog_product_entity_int
-SET value = '2'
-WHERE attribute_id = (SELECT attribute_id FROM eav_attribute WHERE attribute_code = 'status' AND entity_type_id = 4)
-AND entity_id IN (1,2,3);
+UPDATE catalog_product_entity_int 
+SET    value = '2' 
+WHERE  attribute_id = (SELECT attribute_id 
+                       FROM   eav_attribute 
+                       WHERE  attribute_code = 'status' 
+                              AND entity_type_id = 4) 
+       AND entity_id IN ( 1, 2, 3 ); 
--- sql/obfuscateUsers.sql
+++ sql/obfuscateUsers.sql
@@ -1,7 +1,28 @@
 # @author      Andrea De Pirro <andrea.depirro@yameveo.com>
 
-UPDATE `customer_entity` SET `email` = CONCAT ( md5(`email`) ,"@", "example.com");
-UPDATE `sales_flat_order` SET `customer_email` = CONCAT ( SUBSTR(md5(`customer_email`),6) ,"@", "example.com") , `customer_firstname` = md5(`customer_firstname`) , `customer_lastname` = md5(`customer_lastname`);
-UPDATE `sales_flat_order_address` SET `email` = CONCAT ( SUBSTR(md5(`email`),6) ,"@", "example.com") , `firstname` = md5(`firstname`) , `lastname` = md5(`lastname`);
-UPDATE `sales_flat_quote` SET `customer_email` = CONCAT ( SUBSTR(md5(`customer_email`),6) ,"@", "example.com") , `customer_firstname` = md5(`customer_firstname`) , `customer_lastname` = md5(`customer_lastname`);
-UPDATE `sales_flat_quote_address` SET `email` = CONCAT ( SUBSTR(md5(`email`),6) ,"@", "example.com") , `firstname` = md5(`firstname`) , `lastname` = md5(`lastname`);
+UPDATE `customer_entity`
+SET    `email` = CONCAT (MD5(`email`), "@", "example.com");
+
+UPDATE `sales_flat_order`
+SET    `customer_email` = CONCAT (SUBSTR(MD5(`customer_email`), 6), "@",
+                          "example.com")
+       ,
+       `customer_firstname` = MD5(`customer_firstname`),
+       `customer_lastname` = MD5(`customer_lastname`);
+
+UPDATE `sales_flat_order_address`
+SET    `email` = CONCAT (SUBSTR(MD5(`email`), 6), "@", "example.com"),
+       `firstname` = MD5(`firstname`),
+       `lastname` = MD5(`lastname`);
+
+UPDATE `sales_flat_quote`
+SET    `customer_email` = CONCAT (SUBSTR(MD5(`customer_email`), 6), "@",
+                          "example.com")
+       ,
+       `customer_firstname` = MD5(`customer_firstname`),
+       `customer_lastname` = MD5(`customer_lastname`);
+
+UPDATE `sales_flat_quote_address`
+SET    `email` = CONCAT (SUBSTR(MD5(`email`), 6), "@", "example.com"),
+       `firstname` = MD5(`firstname`),
+       `lastname` = MD5(`lastname`);  `firstname` = md5(`firstname`) , `lastname` = md5(`lastname`);
