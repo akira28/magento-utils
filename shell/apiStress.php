3d99f9e2169a74aa38da9997976cf2060f86c04e
andrea <andrea@andrea.newshore>
2012-09-04 17:19:29 +0200

    Added license

--- shell/apiStress.php
+++ shell/apiStress.php
@@ -1,7 +1,22 @@
 <?php
 /**
  * Script to test methods of the Magento API
+ * 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
  *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details at
+ * http://www.gnu.org/copyleft/lgpl.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
  * @author      Andrea De Pirro <andrea.depirro@yameveo.com>
  * @version     1
  */
--- shell/testObserver.php
+++ shell/testObserver.php
@@ -1,14 +1,29 @@
 <?php
-
-require_once 'abstract.php';
-
 /**
  * Magento Compiler Shell Script
+ * 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
  *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details at
+ * http://www.gnu.org/copyleft/lgpl.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
  * @category    Mage
  * @package     Mage_Shell
  * @author      Andrea De Pirro <andrea.depirro@yameveo.com>
  */
+
+require_once 'abstract.php';
+
 class Yameveo_Shell_Example extends Mage_Shell_Abstract
 {
 
