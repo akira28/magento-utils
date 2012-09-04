10fb5125915e9fe871105dd37feb8306abbd745a
andrea <andrea@andrea.newshore>
2012-09-04 13:43:53 +0200

    added magentoCleanup script to reset permissions and clean phisical cache, refactored cleanCache script to be used by magentoCleanup - TODO complete and test some functions

--- shell/cleanCache.php
+++ shell/cleanCache.php
@@ -10,28 +10,24 @@ require_once 'abstract.php';
  * @author      Andrea De Pirro <andreadepirro@gmail.com>
  * @version     1
  */
-class Yameveo_Shell_Cleancache extends Mage_Shell_Abstract
+class Yameveo_Shell_CleanCache extends Mage_Shell_Abstract
 {
 
-    /**
-     * Run script
-     *
-     */
-    public function run()
+    protected function cleanImageCache()
     {
-        ini_set("display_errors", 1);
-        Mage::app('admin')->setUseSessionInUrl(false);
-        Mage::getConfig()->init();
-        
         try {
-            echo "Cleaning image cache... ";
+            echo "Cleaning image cache..." . PHP_EOL;
+            ;
             flush();
             echo Mage::getModel('catalog/product_image')->clearCache();
             echo "[OK]" . PHP_EOL . PHP_EOL;
         } catch (Exception $e) {
             die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
         }
+    }
 
+    protected function cleanDataCache()
+    {
         try {
             echo "Cleaning data cache..." . PHP_EOL;
             flush();
@@ -45,18 +41,13 @@ class Yameveo_Shell_Cleancache extends Mage_Shell_Abstract
         } catch (Exception $e) {
             die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
         }
+    }
 
+    protected function cleanMergedJSCSS()
+    {
         try {
-            echo "Cleaning stored cache... ";
-            flush();
-            echo Mage::app()->getCacheInstance()->clean() ? "[OK]" : "[ERROR]";
-            echo PHP_EOL . PHP_EOL;
-        } catch (Exception $e) {
-            die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
-        }
-
-        try {
-            echo "Cleaning merged JS/CSS...";
+            echo "Cleaning merged JS/CSS..." . PHP_EOL;
+            ;
             flush();
             Mage::getModel('core/design_package')->cleanMergedJsCss();
             Mage::dispatchEvent('clean_media_cache_after');
@@ -64,35 +55,86 @@ class Yameveo_Shell_Cleancache extends Mage_Shell_Abstract
         } catch (Exception $e) {
             die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
         }
+    }
 
+    protected function cleanStoredCache()
+    {
         try {
-            echo "Cleaning physical files...";
+            echo "Cleaning stored cache..." . PHP_EOL;
+            ;
             flush();
-            $dir = Mage::getBaseDir('cache');
-            $items = array_diff(scandir($dir), array('..', '.'));
-            foreach ($items as $item) {
-                $path = $dir . DIRECTORY_SEPARATOR . $item;
-                is_dir($path) ? $this->_rrmdir($path) : unlink($path);
-            }
+            echo Mage::app()->getCacheInstance()->clean() ? "[OK]" : "[ERROR]";
+            echo PHP_EOL . PHP_EOL;
+        } catch (Exception $e) {
+            die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
+        }
+    }
+
+    protected function cleanPhisicalCache()
+    {
+        // @todo verify these functions
+        try {
+            echo "Cleaning physical files..." . PHP_EOL;
+            ;
+            flush();
+            $this->_rrmdirContent(Mage::getBaseDir('cache'));
+            $this->_rrmdirContent(Mage::getBaseDir('full_page_cache'));
+            $this->_rrmdirContent(Mage::getBaseDir('minifycache'));
+            $this->_rrmdirContent(Mage::getBaseDir('session'));
             echo "[OK]" . PHP_EOL . PHP_EOL;
         } catch (Exception $e) {
             die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
         }
+    }
 
-        if (function_exists('accelerator_reset')) {
-            try {
-                echo "Cleaning accelerator...";
-                flush();
-                accelerator_reset();
-                echo "[OK]" . PHP_EOL . PHP_EOL;
-            } catch (Exception $e) {
-                die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
-            }
+    protected function cleanAccelerator()
+    {
+        try {
+            echo "Cleaning accelerator...";
+            flush();
+            accelerator_reset();
+            echo "[OK]" . PHP_EOL . PHP_EOL;
+        } catch (Exception $e) {
+            die("[ERROR:" . $e->getMessage() . "]" . PHP_EOL);
         }
     }
 
     /**
-     * Remove a directory and all elements contained
+     * Run script
+     *
+     */
+    public function run()
+    {
+        ini_set("display_errors", 1);
+        Mage::app('admin')->setUseSessionInUrl(false);
+        Mage::getConfig()->init();
+
+        $this->cleanImageCache();
+        $this->cleanDataCache();
+        $this->cleanStoredCache();
+        $this->cleanMergedJSCSS();
+        $this->cleanPhisicalCache();
+
+        if (function_exists('accelerator_reset')) {
+            $this->cleanAccelerator();
+        }
+    }
+
+    /**
+     * Removes all elements contained in the given directory
+     * @param string $dir directory containing elements to remove
+     */
+    private function _rrmdirContent($dir)
+    {
+        $items = array_diff(scandir($dir), array('..', '.'));
+        foreach ($items as $item) {
+            $path = $dir . DIRECTORY_SEPARATOR . $item;
+            is_dir($path) ? $this->_rrmdir($path) : unlink($path);
+        }
+    }
+
+    /**
+     * Removes a directory and all elements contained
      * @param string $dir directory to remove
      */
     private function _rrmdir($dir)
@@ -121,5 +163,5 @@ USAGE;
 
 }
 
-$shell = new Yameveo_Shell_Cleancache();
+$shell = new Yameveo_Shell_CleanCache();
 $shell->run();
---
+++ shell/magentoCleanup.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'cleanCache.php';
+
+/**
+ * Reset files and directories permissions and clean all the caches
+ *
+ * @category    Mage
+ * @package     Mage_Shell
+ * @author      Andrea De Pirro <andreadepirro@gmail.com>
+ * @version     1
+ */
+class Yameveo_Shell_Cleanup extends Yameveo_Shell_CleanCache
+{
+    /**
+     * Function to set file permissions to 0644 and folder permissions to 0755
+     * @param type $dir
+     * @param type $dirModes
+     * @param type $fileModes
+     */
+    private function fixPermissions($dir = "./", $dirModes = 0755, $fileModes = 0644)
+    {
+        echo "Setting all folder permissions to 755" . PHP_EOL;
+        echo "Setting all file permissions to 644" . PHP_EOL;
+        $d = new RecursiveDirectoryIterator($dir);
+        foreach (new RecursiveIteratorIterator($d, 1) as $path) {
+            if ($path->isDir())
+                chmod($path, $dirModes);
+            else if (is_file($path))
+                chmod($path, $fileModes);
+        }
+    }
+   
+    /**
+     * Run script
+     *
+     */
+    public function run()
+    {
+        $this->fixPermissions(Mage::getBaseDir());
+        echo "Setting mage permissions to 550<br/>";
+        chmod("mage", 0550);
+        echo "Setting var, var/.htaccess, app/etc permissions to o+w";
+        // @todo verify these
+        chmod(Mage::getBaseDir('var'), 0666 );
+        chmod(Mage::getBaseDir('var') . "/.htaccess", 0666 );
+        chmod(Mage::getBaseDir('app') . "/etc", 0666 );
+        
+        // @todo change permissions to sh scripts
+        
+        $this->cleanPhisicalCache();
+    }
+
+}
+
+
+$shell = new Yameveo_Shell_Cleanup();
+$shell->run();
