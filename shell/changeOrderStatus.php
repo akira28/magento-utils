d5ade542dcbb7c196c57ef8cec47ad167af1c9f0
andrea <andrea@andrea.newshore>
2012-09-04 16:20:15 +0200

    Cleaned up changeOrderStatus - TODO make it more "magento shell" and parametric

--- shell/changeOrderStatus.php
+++ shell/changeOrderStatus.php
@@ -1,21 +1,14 @@
 <?php
-
+// Close every order before dateTo
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once('../app/Mage.php');
 Mage::app('admin');
-// cambiar de estado de 10 dias antes
+$dateTo = '16 June 2012';
 $orders = Mage::getModel('sales/order')
         ->getCollection()
-        ->addAttributeToFilter('created_at', array('to' => '16 June 2012', 'date' => true))
+        ->addAttributeToFilter('created_at', array('to' => $dateTo, 'date' => true))
         ->addAttributeToFilter('status', 'shipped');
 foreach($orders as $order) {
     $order->setStateUnprotected('complete')->setStatus('complete')->save();
-}
-$cb_dhl_shipments = Mage::getModel('Cb_Dhl/shipment')
-        ->getCollection()
-        ->addFieldToFilter('created_at', array('to' => '16 June 2012', 'date' => true))
-        ->addFieldToFilter('closed', 0);
-foreach($cb_dhl_shipments as $shipment) {
-    $shipment->setClosed('1')->setClosedAt(now())->save();
 }
