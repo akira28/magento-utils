e2a934a2cd078b3aee15bf89078954e483ae37ee
Andrea De Pirro <andrea.depirro@yameveo.com>
2013-10-07 15:36:07 +0200

    Fix for obfuscateUsers.sql script

--- sql/obfuscateUsers.sql
+++ sql/obfuscateUsers.sql
@@ -25,4 +25,4 @@ SET    `customer_email` = CONCAT (SUBSTR(MD5(`customer_email`), 6), "@",
 UPDATE `sales_flat_quote_address`
 SET    `email` = CONCAT (SUBSTR(MD5(`email`), 6), "@", "example.com"),
        `firstname` = MD5(`firstname`),
-       `lastname` = MD5(`lastname`);  `firstname` = md5(`firstname`) , `lastname` = md5(`lastname`);
+       `lastname` = MD5(`lastname`);
